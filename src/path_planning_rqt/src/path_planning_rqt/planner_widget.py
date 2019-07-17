4173e63d2b06421da91ed573056b6a68eddc1ad2
William Le <willle3456@gmail.com>
2019-07-17 16:20:32 -0700

    fixed object positioning bug, cleaned up code, and added field boundaries

--- src/path_planning_rqt/src/path_planning_rqt/planner_widget.py
+++ src/path_planning_rqt/src/path_planning_rqt/planner_widget.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env  python
 
-from python_qt_binding.QtWidgets import QWidget, QGraphicsView, QGraphicsScene, QGraphicsEllipseItem, QGraphicsLineItem, QGraphicsItemGroup
-from python_qt_binding.QtGui import QPainter, QPen, QBrush, QColor, QTransform
+from python_qt_binding.QtWidgets import QWidget, QGraphicsView, QGraphicsScene, QGraphicsEllipseItem, QGraphicsLineItem, QGraphicsRectItem
+from python_qt_binding.QtGui import QPainter, QPen, QBrush, QColor
 from python_qt_binding.QtCore import Signal
 from python_qt_binding import loadUi
 import networkx as nx
@@ -28,10 +28,11 @@ class PlannerViewerWidget(QWidget):
         # scene setup
         self.scene = QGraphicsScene()
         self.scene.setBackgroundBrush(QBrush(QColor(0,255,0)))
-        #tmp_item = QGraphicsEllipseItem(0, 0, 100, 100)
-        #self.scene.addItem(tmp_item)
+        self.field_rect = QGraphicsRectItem(0, 0, 900, 600)
+        self.field_rect.moveBy(-450, -300)
+        self.field_rect.setPen(QPen(QColor(255,255,255)))
+        self.scene.addItem(self.field_rect)
         self.planner_viewer.setScene(self.scene)
-        #self.planner_viewer.show()
 
         # class objects
         self.graph = nx.Graph()
@@ -56,39 +57,22 @@ class PlannerViewerWidget(QWidget):
         self.graph_update.connect(self.draw_graph)
         self.position_update.connect(self.draw_obs)
 
-        # transforms
-        self.widget_tf = QTransform()
-        self.widget_tf.scale(0.05, 0.05)
-        #self.widget_tf.translate(450, 300)
-
         self.bot_size = 15
-        self.last_time = time.time()
-        self.avg_buff = [0.0 for i in range(20)]
-        self.avg_idx = 0
-
-        # item groups
-        self.edge_group = QGraphicsItemGroup()
-        self.path_group = QGraphicsItemGroup()
-
-        self.scene.addItem(self.edge_group)
-        self.scene.addItem(self.path_group)
 
     def update_graph(self, graph):
         self.graph = graph
 
     def get_msg_position(self, robot):
-        return robot.r_pose.position.x/20, -robot.r_pose.position.y/20
+        return (robot.r_pose.position.x/10), (-robot.r_pose.position.y/10)
 
     def get_item_position(self, item):
         return item.x(), item.y()
 
     def add_item(self, robot, item_list, brush, pen):
         x, y = self.get_msg_position(robot)
-
-        new_item = QGraphicsEllipseItem(x, y, self.bot_size, self.bot_size)
+        new_item = QGraphicsEllipseItem(-self.bot_size/2, -self.bot_size/2, self.bot_size, self.bot_size)
         new_item.setBrush(brush)
         new_item.setPen(pen)
-        #new_item.setTransform(self.widget_tf)
 
         item_list.update({robot.id: new_item})
         self.scene.addItem(item_list[robot.id])
@@ -98,7 +82,7 @@ class PlannerViewerWidget(QWidget):
         last_x, last_y = self.get_item_position(item)
         dx = x - last_x
         dy = y - last_y
-        
+
         item.moveBy(dx, dy)
 
     def update_items(self, data, item_list, brush, pen):
@@ -112,30 +96,14 @@ class PlannerViewerWidget(QWidget):
     def draw_obs(self, position_data):
         self.update_items(position_data.friends, self.team, self.team_brush, self.team_pen)
         self.update_items(position_data.enemies, self.obs, self.opp_brush, self.opp_pen)
+
         self.scene.update()
 
-        #curr_time = time.time()
-        #self.avg_buff[self.avg_idx] = min(100.0, 1/(curr_time - self.last_time))
-        #self.avg_idx = (self.avg_idx + 1)%20
-        #print 1/(curr_time - self.last_time)
-        #self.last_time = curr_time
-
-        #print sum(self.avg_buff)/20.0
-        #print self.avg_buff
-
-        #print 'TEAM:\n'
-        #print [(t.x(), t.y()) for t in self.team.values()]
-        #print 'OBS:\n'
-        #print [(o.x(), o.y()) for o in self.obs.values()]
-
     def draw_nodes(self):
-        # remove the old (can find by position)
-        # add the new
         for node in list(self.graph.nodes()):
             self.scene.addEllipse(node.x, node.y, 2, 2)
 
         self.scene.update()
-        #self.render(self.painter)
 
     def draw_graph(self, graph_msg):
         #self.draw_nodes()
@@ -143,8 +111,8 @@ class PlannerViewerWidget(QWidget):
             self.scene.removeItem(item)
         self.path = []
 
-        for edge_from, edge_to in zip(graph_msg.new_edges[::2], graph_msg.new_edges[1::2]):
-            tmp_line = QGraphicsLineItem(edge_from.position.x/10, -edge_from.position.y/10, edge_to.position.x/10, -edge_to.position.y/10)
+        for start, end in zip(graph_msg.new_edges_from, graph_msg.new_edges_to):
+            tmp_line = QGraphicsLineItem(start.position.x/10, -start.position.y/10, end.position.x/10, -end.position.y/10)
             tmp_line.setPen(self.graph_pen)
             self.scene.addItem(tmp_line)
         
@@ -152,7 +120,7 @@ class PlannerViewerWidget(QWidget):
             tmp_line = QGraphicsLineItem(start_way.position.x/10, -start_way.position.y/10, end_way.position.x/10, -end_way.position.y/10)
             tmp_line.setPen(self.path_pen)
             self.path.append(tmp_line)
-    
+        
         for item in self.path:
             self.scene.addItem(item)
         
